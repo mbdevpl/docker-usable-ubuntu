08d99401cacbd4329ee64b5c835f8164f800b930
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-04-23 11:15:46 +0900

    fix pip installation on 18.04

--- init_ubuntu_base.sh
+++ init_ubuntu_base.sh
@@ -102,7 +102,13 @@ python2 --version || echo "not available"
 python3 --version
 python3.6 --version
 
-curl https://bootstrap.pypa.io/get-pip.py | python3.6
+if [[ "${UBUNTU_RELEASE}" == "16.04" ]] ; then
+  curl https://bootstrap.pypa.io/get-pip.py | python3.6
+else
+  apt install -y python3-pip
+  update-alternatives --install /usr/bin/pip3.6 pip3.6 /usr/bin/pip3 36
+fi
+
 pip3.6 install -U pip setuptools wheel twine
 
 pip --version || echo "not available"
