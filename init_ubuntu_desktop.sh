6154e7fbe3c8a27102aa8ebdf60730fb28f53ad5
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-04-13 15:08:08 +0900

    software for Atom

--- init_ubuntu_desktop.sh
+++ init_ubuntu_desktop.sh
@@ -70,6 +70,9 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' && apt update
 apt install -y atom
 
+apt install -y cppcheck  # linter-cppcheck
+apt install -y devscripts  # linter-checkbashisms
+
 # travis
 
 gem install --user-install travis --no-rdoc --no-ri
