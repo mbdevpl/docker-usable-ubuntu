33fd6022b58de77c3af97edf9bc255f814aca619
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2021-01-14 13:42:32 +0900

    add missing clangd-10 package

--- init_llvm.sh
+++ init_llvm.sh
@@ -8,7 +8,7 @@ UBUNTU_CODENAME="$(lsb_release -cs)"
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 add-apt-repository "deb http://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-10 main" && apt-get update
-apt-get install --no-install-recommends -y llvm-10 llvm-10-dev clang-10 libclang-10-dev lldb-10 clang-tools-10
+apt-get install --no-install-recommends -y llvm-10 llvm-10-dev clang-10 libclang-10-dev clangd-10 lldb-10 clang-tools-10
 
 update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-10 10
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 10
